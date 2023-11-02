2109208237f1c4963555e6f388997323efcbc44b
Milan Lempera <milan.lempera@gmail.com>
2023-11-02 21:12:08 +0100

    chore: update deps

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.12.0"]]}}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]]}}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.github.seancorfield/expectations "2.1.188"]]
   :main coderetreat-clojure-sandbox.core)
--- test/coderetreat_clojure_sandbox/core_test.clj
+++ test/coderetreat_clojure_sandbox/core_test.clj
@@ -1,7 +1,18 @@
 (ns coderetreat-clojure-sandbox.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest testing is]]
+            [expectations.clojure.test
+             :refer [defexpect expect expecting]]
             [coderetreat-clojure-sandbox.core :refer :all]))
+
+; https://cljdoc.org/d/com.github.seancorfield/expectations/2.1.188/doc/readme
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(defexpect equality
+  (expect 1 (* 1 1))
+  (expect "foo" (str "f" "oo")))
+
+(defexpect regex-1
+  (expect #"foo" "It's foobar!"))
